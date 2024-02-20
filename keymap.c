778a2393af69b780c04f5a627697df01b7d25467
Kentaro Kawano <kentaro.kawano@gmail.com>
2024-02-20 09:28:22 +0900

    ロータリエンコーダーの挙動を修正

--- keymap.c
+++ keymap.c
@@ -56,7 +56,7 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     } else {
         tap_code(KC_VOLU);  //Rotary encoder Reverse clockwise
     }
-    return true;
+    return false;
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
